39c30ad973160d9247bc4efe9c87f8757d480a52
bridgetshroom <160240629@student.rnngroup.ac.uk>
2024-09-20 14:03:55 +0100

    updating website files

--- dcrm/website/urls.py
+++ dcrm/website/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+
     path('', views.home, name=""),
+
+    path('register', views.register, name="register"),
+
 ]
--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -1,4 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import CreateUserForm, LoginForm
 
 def home(request):
     return render(request, 'website/index.html')
+
+# register a user
+def register(request):
+    form = CreateUserForm()
+
+    if request.method == "POST":
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('')
+
+    context = {'form': form}
+
+    return render(request,'website/register.html', context=context)
