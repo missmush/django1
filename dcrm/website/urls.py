f3027c8c852c6cf136245471122b9e1045e39ad4
bridgetshroom <160240629@student.rnngroup.ac.uk>
2024-09-23 14:27:35 +0100

    completed up to slide 57

--- dcrm/website/urls.py
+++ dcrm/website/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
 
     path('register', views.register, name="register"),
 
+    path('my-login', views.my_login, name="my-login"),
+
+    path('user-logout', views.user_logout, name= "user-logout"),
+
 ]
--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, redirect
 from .forms import CreateUserForm, LoginForm
 
+from django.contrib.auth.models import auth
+from django.contrib.auth import authenticate
+
+
 def home(request):
     return render(request, 'website/index.html')
 
@@ -12,8 +16,32 @@ def register(request):
         form = CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('')
+            return redirect('my-login')
 
     context = {'form': form}
-
     return render(request,'website/register.html', context=context)
+
+# login a user
+def my_login(request):
+    form = LoginForm
+    
+    if request.method == "POST":
+        form = LoginForm(request, data=request.POST)
+
+        if form.is_valid():
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:
+                auth.login(request, user)
+                
+        context = {'form': form}
+        return render(request,'website/register.html', context=context)
+    
+# logout a user
+def user_logout(request):
+
+    auth.logout(request)
+    return redirect("my-login")
