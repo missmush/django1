7d31c55f40681ff5463dac66ac9345f5da9abc59
bridgetshroom <160240629@student.rnngroup.ac.uk>
2024-10-04 14:42:18 +0100

    completed django 2 slides

--- dcrm/website/urls.py
+++ dcrm/website/urls.py
@@ -12,4 +12,18 @@ urlpatterns = [
     path('user-logout', views.user_logout, name= "user-logout"),
 
     path('dashboard', views.dashboard, name="dashboard"),
-]
+
+    ### CRUD ###
+    #read
+    path('dashboard',views.dashboard, name="dashboard"),
+
+    #create
+    path('create-record', views.create_record, name="create-record"),
+
+    #update
+    path('update-record/<int:pk>', views.update_record, name="update-record"),
+
+    #view a single record
+    path('record/<int:pk>', views.singular_record, name="record"),
+    
+    ]
--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .forms import CreateUserForm, LoginForm
+from .forms import CreateUserForm, LoginForm, CreateRecordForm, UpdateRecordForm
 
 from django.contrib.auth.models import auth
 from django.contrib.auth import authenticate
@@ -57,3 +57,48 @@ def dashboard(request):
     my_records = Record.objects.all()
     context = {'records': my_records}
     return render(request, 'website/dashboard.html', context=context)
+
+# create a record
+@login_required(login_url='my_login')
+def create_record(request):
+
+    form = CreateRecordForm()
+    if request.method == "POST":
+        form = CreateRecordForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect("dashboard")
+        
+    context = {'create_form': form}
+    return render(request, 'website/create-record.html',context=context)
+
+# update a record
+@login_required(login_url='my-login')
+def update_record(request, pk):
+
+    record = Record.objects.get(id=pk)
+    form= UpdateRecordForm(instance=record)
+
+    if request.method == "POST":
+        form = UpdateRecordForm(request.POST, instance=record)
+
+        if form.is_valid():
+            form.save()
+            return redirect("dashboard")
+        
+        context = {'update_form': form}
+        return render(request, 'website/update-record.html',context=context)
+
+# read a single record
+@login_required(login_url='my-login')
+def singular_record(request, pk):
+    
+    one_record = Record.objects.get(id=pk)
+    context = {'record':one_record}
+    return render(request, 'website/view-record.html', context = context)
+
+# delete a record
+@login_required(login_url='my-login')
+def delete_record(request, pk):
+    record = Record.objects.get
